01b8dd7309f8f3bfb0b6dd90da362e3504e5b2de
PCBstreiker <PCBstreiker@gmail.com>
2024-02-21 10:12:39 +0000

    Anpassungen aller Layouts

--- Industrie40/src/application/Main.java
+++ Industrie40/src/application/Main.java
@@ -2,8 +2,10 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -20,6 +22,12 @@ public class Main extends Application {
 		   
 		   Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
 		   Scene scene = new Scene(root);
+		   Screen screen = Screen.getPrimary();
+		   Rectangle2D bounds = screen.getVisualBounds();
+		   stage.setX(bounds.getMinX());
+		   stage.setY(bounds.getMinY());
+		   stage.setWidth(bounds.getWidth());
+		   stage.setHeight(bounds.getHeight());
 		   stage.setScene(scene);
 		   stage.show();
 		   
